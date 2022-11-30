2abb80eb17f38e1cfc129df0c29fa0f3ed2edebf
Fabian Soriano <fabo.soriano@gmail.com>
2022-11-29 18:19:56 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+// Engine used and requiring JSX so we can build our views
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to BREADS APP')
